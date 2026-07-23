eefd43693171c741b81897c5d9387ead32d7fb25
Kayro Renzo <kayrorenzo08@gmail.com>
2026-07-23 16:56:42 -0300

    Delete src/Projetos_POO/src directory

--- src/Projetos_POO/src/Projetos_POO/GerenciadorDeTarefasComPrioridade/model/Prioridade.java
+++
@@ -1,9 +0,0 @@
-package POO.Projetos_POO.GerenciadorDeTarefasComPrioridade.model;
-
-public enum Prioridade {
-    BAIXA,      // Pode esperar
-    MEDIA,      // Deve ser feita em breve
-    ALTA,       // Urgente
-    CRITICA,     // Impacto severo se não feita
-    URGENTE
-}
--- src/Projetos_POO/src/Projetos_POO/GerenciadorDeTarefasComPrioridade/model/Status.java
+++
@@ -1,9 +0,0 @@
-package POO.Projetos_POO.GerenciadorDeTarefasComPrioridade.model;
-
-public enum Status {
-    PENDENTE,        // Aguardando início
-    EM_ANDAMENTO,    // Em execução
-    BLOQUEADA,       // Aguardando dependência
-    CONCLUIDA,       // Finalizada
-    CANCELADA        // Não será feita
-}
